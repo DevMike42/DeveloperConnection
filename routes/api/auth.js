e7b39842ffdccf353f8e42ebe52abbf074e630d6
Mike Rivera <michaelstephenrivera@gmail.com>
2019-09-20 13:15:21 -0700

    Created Login route for user

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
 const auth = require('../../middleware/auth');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const { check, validationResult } = require('express-validator');
 
 const User = require('../../models/User');
 
@@ -16,5 +20,71 @@ router.get('/', auth, async (req, res) => {
         res.status(500).send('Server Error');
     }
 });
+
+// @route   POST api/auth
+// @desc    Authenticate user & get token
+// @access  Public
+router.post(
+    '/',
+    [
+        check('email', 'Please include a valid email address').isEmail(),
+        check('password', 'Password is required').exists()
+    ],
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        const { email, password } = req.body; // Desctructures req.body so we don't have to keep typing it
+
+        try {
+            let user = await User.findOne({ email });
+
+            // See if User exists
+            if (!user) {
+                return res
+                    .status(400)
+                    .json({ errors: [{ msg: 'Invalid Credentials' }] });
+            }
+
+            // the hashed password from the db using the .compare method in bcrypt
+            const isMatch = await bcrypt.compare(password, user.password)
+
+            if (!isMatch) {
+                return res
+                    .status(400)
+                    .json({ errors: [{ msg: 'Invalid Credentials' }] });
+            }
+
+            // Compares the plain text user input password to
+
+
+            // Return JSON Web Token
+
+            // Stores the payload for the jwt grabbing the id from the User 
+            const payload = {
+                user: {
+                    id: user.id
+                }
+            }
+
+            // Sign JWT
+            jwt.sign(
+                payload, // Passes in the payload
+                config.get('jwtSecret'), // Passes in the Secret
+                { expiresIn: 360000 }, // Sets an expiration period (Need to change back to 3600 (1 hour))
+                (err, token) => {
+                    if (err) throw err;
+                    res.json({ token }); // If no err, send token back to client
+                });
+
+        } catch (err) {
+            console.error(err.message);
+            res.status(500).send('Server Error');
+        }
+
+    }
+);
 
 module.exports = router;
