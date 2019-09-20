59987896e513ba4c8d02b2594352d71432bf3a07
Mike Rivera <michaelstephenrivera@gmail.com>
2019-09-20 09:45:35 -0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => res.send('AIP Running'));
+
+const PORT = process.env.PORT || 5000;
+// process.env.PORT will listen for an environment variable called PORT when depoyed to Heroku
+// Locally it will run on PORT 5000
+
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
