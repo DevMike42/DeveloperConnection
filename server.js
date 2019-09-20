9a18f39e024bcade89087ab8f4c7abc0ffc23bdd
Mike Rivera <michaelstephenrivera@gmail.com>
2019-09-20 10:35:36 -0700

    Created basic routes for testing endpoints

--- server.js
+++ server.js
@@ -8,6 +8,12 @@ connectDB();
 
 app.get('/', (req, res) => res.send('AIP Running'));
 
+// Define routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
+
 const PORT = process.env.PORT || 5000;
 // process.env.PORT will listen for an environment variable called PORT when depoyed to Heroku
 // Locally it will run on PORT 5000
