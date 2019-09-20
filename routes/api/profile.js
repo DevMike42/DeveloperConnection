44ce4b76f3585ae9a94d753b36c32baa856aaddb
Mike Rivera <michaelstephenrivera@gmail.com>
2019-09-20 16:52:51 -0700

    Created profile routes for deleting user and profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -133,7 +133,7 @@ router.get('/user/:user_id', async (req, res) => {
     try {
         const profile = await Profile.findOne({ user: req.params.user_id }).populate('user', ['name', 'avatar']);
 
-        if (!profile) return res.status(400).json({ msg: 'There is no profile for this user' });
+        if (!profile) return res.status(400).json({ msg: 'Profile not found' });
 
         res.json(profile);
     } catch (err) {
@@ -141,6 +141,24 @@ router.get('/user/:user_id', async (req, res) => {
         if (err.kind == 'ObjectId') {
             return res.status(400).json({ msg: 'Profile not found' });
         }
+        res.status(500).send('Server Error');
+    }
+});
+
+// @route   DELETE api/profile
+// @desc    Delete profile, user, and posts
+// @access  Private
+router.delete('/', auth, async (req, res) => {
+    try {
+        // @todo - Remove users posts
+        // Remove profile 
+        await Profile.findOneAndRemove({ user: req.user.id });
+        // Remove user
+        await User.findOneAndRemove({ _id: req.user.id });
+
+        res.json({ msg: 'User Removed' });
+    } catch (err) {
+        console.error(err.message);
         res.status(500).send('Server Error');
     }
 });
