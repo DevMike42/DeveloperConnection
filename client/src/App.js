4a23b47c5ce87dd32b425a514d2bcb8667ed1e3b
Mike Rivera <michaelstephenrivera@gmail.com>
2019-09-25 08:28:21 -0700

    unsaved mistake

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
 import React, { Fragment, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import setAuthToken from './utils/setAuthToken';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
@@ -11,6 +10,7 @@ import Alert from './components/layout/Alert';
 import { Provider } from 'react-redux';
 import store from './store';
 import { loadUser } from './actions/auth';
+import setAuthToken from './utils/setAuthToken';
 
 import './App.css';
 
