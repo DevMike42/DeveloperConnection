fc1e04913d9def054508db20e46ac0294e600a2b
Mike Rivera <michaelstephenrivera@gmail.com>
2019-09-29 07:50:12 -0700

    Completed Profile Actions and Reducer

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -4,10 +4,12 @@ import { setAlert } from './alert';
 // Bring in action types to be used
 import {
     GET_PROFILE,
+    GET_PROFILES,
     PROFILE_ERROR,
     UPDATE_PROFILE,
     CLEAR_PROFILE,
-    ACCOUNT_DELETED
+    ACCOUNT_DELETED,
+    GET_REPOS
 } from './types';
 
 // Get curent user's profile
@@ -17,6 +19,59 @@ export const getCurrentProfile = () => async dispatch => {
 
         dispatch({
             type: GET_PROFILE,
+            payload: res.data,
+        });
+    } catch (err) {
+        dispatch({
+            type: PROFILE_ERROR,
+            payload: { msg: err.response.statusText, status: err.response.status }
+        });
+    }
+};
+
+// Get all profiles
+export const getProfiles = () => async dispatch => {
+    dispatch({ type: CLEAR_PROFILE });
+
+    try {
+        const res = await axios.get('api/profile');
+
+        dispatch({
+            type: GET_PROFILES,
+            payload: res.data,
+        });
+    } catch (err) {
+        dispatch({
+            type: PROFILE_ERROR,
+            payload: { msg: err.response.statusText, status: err.response.status }
+        });
+    }
+};
+
+// Get profile by id
+export const getProfileById = userId => async dispatch => {
+    try {
+        const res = await axios.get(`api/profile/user/${userId}`);
+
+        dispatch({
+            type: GET_PROFILE,
+            payload: res.data,
+        });
+    } catch (err) {
+        dispatch({
+            type: PROFILE_ERROR,
+            payload: { msg: err.response.statusText, status: err.response.status }
+        });
+    }
+};
+
+// Get Github Repos
+export const getGithubRepos = username => async dispatch => {
+    try {
+        const res = await axios.get(`api/profile/github/${username}`);
+
+        dispatch({
+            type: GET_REPOS,
             payload: res.data,
         });
     } catch (err) {
