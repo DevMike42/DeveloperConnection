de867f1a660af7dd3e315a7765148afe564c5f9b
Mike Rivera <michaelstephenrivera@gmail.com>
2019-10-01 18:41:58 -0700

    Created display functionality for ProfileTop and ProfileAbout

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -3,6 +3,8 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
+import ProfileTop from './ProfileTop';
+import ProfileAbout from './ProfileAbout';
 import { getProfileById } from '../../actions/profile';
 
 const Profile = ({ getProfileById, profile: { profile, loading }, auth, match }) => {
@@ -11,12 +13,30 @@ const Profile = ({ getProfileById, profile: { profile, loading }, auth, match })
         getProfileById(match.params.id);
     }, [getProfileById, match.params.id]);
 
-    return <Fragment>
-        {profile === null || loading ? <Spinner /> : <Fragment>
-            <Link to='/profiles' className='btn btn-light'>Back to Profiles</Link>
-            {auth.isAuthenticated && auth.loading === false && auth.user._id === profile.user._id && (<Link to='/edit-profile' className='btn btn-dark'>Edit Profile</Link>)}
-        </Fragment>}
-    </Fragment>
+    return (
+        <Fragment>
+            {profile === null || loading ? (
+                <Spinner />
+            ) : (
+                    <Fragment>
+                        <Link to='/profiles' className='btn btn-light'>
+                            Back to Profiles
+                    </Link>
+                        {auth.isAuthenticated &&
+                            auth.loading === false &&
+                            auth.user._id === profile.user._id && (
+                                <Link to='/edit-profile' className='btn btn-dark'>
+                                    Edit Profile
+                        </Link>
+                            )}
+                        <div className="profile-grid my-1">
+                            <ProfileTop profile={profile} />                            <ProfileTop profile={profile} />
+                            <ProfileAbout profile={profile} />
+                        </div>
+                    </Fragment>
+                )}
+        </Fragment>
+    );
 };
 
 Profile.propTypes = {
